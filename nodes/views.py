b96dfe2c44cc664b0e30e482bf05972300201c9c
ghostforpy <ghostformobile@gmail.com>
2023-02-26 20:02:07 +0300

    add paginator and search nodes

--- nodes/views.py
+++ nodes/views.py
@@ -1,9 +1,11 @@
-# from django.shortcuts import render
-# from django.views.generic import ListView
-from django.views.generic.edit import FormView, CreateView, UpdateView
-from django.db.models import Q
+from datetime import datetime
 
-from .models import Node, NodeName
+from django.views.generic.edit import FormView, CreateView, UpdateView
+from django.core.paginator import Paginator
+from abonents.models import ObjectStatus
+from buildings.models import Building, Region
+
+from .models import Node, NodeName, NodeType
 
 
 create_and_update_fileds = [
@@ -26,22 +28,75 @@ class ListAndCreateNodeView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         qs = self.model.objects.all()
-        if self.request.GET.get('query'):
-            query = self.request.GET.get('query')
-            context["query"] = query
-            qs = qs.filter(
-                Q(name__name__icontains=query) |
-                Q(type__name__icontains=query) |
-                Q(entity__name__icontains=query) |
-                Q(status__name__icontains=query) |
-                Q(ip_address__icontains=query) |
-                Q(parent__name__name__icontains=query) |
-                Q(building__type__name__icontains=query) |
-                Q(building__street__icontains=query) |
-                Q(building__region__name__icontains=query)
-            )
+
+        if self.request.GET.get('type'):
+            _type = self.request.GET.get('type')
+            if _type.isdigit():
+                qs = qs.filter(type__id=int(_type))
+                context['select_type'] = int(_type)
+        if self.request.GET.get('name'):
+            name = self.request.GET.get('name')
+            if name.isdigit():
+                qs = qs.filter(name__id=int(name))
+                context['select_name'] = int(name)
+        if self.request.GET.get('building'):
+            building = self.request.GET.get('building')
+            if building.isdigit():
+                qs = qs.filter(building__id=int(building))
+                context['select_building'] = int(building)
+        if self.request.GET.get('ip_address'):
+            ip_address = self.request.GET.get('ip_address')
+            qs = qs.filter(ip_address__icontains=ip_address)
+            context['ip_address'] = ip_address
+        if self.request.GET.get('object_status'):
+            object_status = self.request.GET.get('object_status')
+            if object_status.isdigit():
+                qs = qs.filter(status__id=int(object_status))
+                context['select_object_status'] = int(object_status)
+        if self.request.GET.get('created_date'):
+            created_date = self.request.GET.get('created_date')
+            try:
+                qs = qs.filter(
+                    created_date=datetime.strptime(created_date, "%Y-%m-%d")
+                )
+                context['created_date'] = created_date
+            except (ValueError, TypeError):
+                pass
+        if self.request.GET.get('change_date'):
+            change_date = self.request.GET.get('change_date')
+            try:
+                qs = qs.filter(
+                    change_date=datetime.strptime(change_date, "%Y-%m-%d")
+                )
+                context['change_date'] = change_date
+            except (ValueError, TypeError):
+                pass
+        if self.request.GET.get('parent_node'):
+            parent_node = self.request.GET.get('parent_node')
+            if parent_node.isdigit():
+                qs = qs.filter(parent__id=int(parent_node))
+                context['parent_node'] = int(parent_node)
+        if self.request.GET.get('region'):
+            region = self.request.GET.get('region')
+            if region.isdigit():
+                qs = qs.filter(building__region__id=int(region))
+                context['select_region'] = int(region)
+
+        page = 1
+        if self.request.GET.get('page'):
+            page = self.request.GET.get('page')
+            if page.isdigit():
+                page = int(page)
+        paginator = Paginator(qs, 20)
+        context['nodes_pages'] = paginator.num_pages
+        context["nodes"] = paginator.get_page(page)
+
+        context["object_statuses"] = ObjectStatus.objects.all()
+        context['building_types'] = Building.objects.all()
         context['node_names'] = NodeName.objects.select_related('type').all()
-        context["nodes"] = qs
+        context["node_types"] = NodeType.objects.all()
+        context["parent_nodes"] = Node.objects.all()
+        context["regions"] = Region.objects.all()
         return context
 
 
